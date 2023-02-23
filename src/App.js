12635e74e885e13558287c208593005ca0128a2b
Ashish Garg <45831857+gargashi@users.noreply.github.com>
2023-02-23 11:03:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>recipe</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+`;
+const Nav = styled.div`
+  display: flex;
+  padding: 4rem 0rem;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
+
+export default App;
